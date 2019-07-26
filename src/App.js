8bf62e88940f84f85a9c3552cb3d8940b5b454b3
Alex van Stekelenborg <35575349+AlStek@users.noreply.github.com>
2019-07-26 19:27:17 -0400

    Revert "Merge branch 'master' of https://github.com/vanstek/PowerChart-Web"
    This reverts commit 21479903f338a8a90332dfe56a88e51063212bbe, reversing
    changes made to 401e1fcbcd92b6e9ec6de0aa494f363b7272e6b0.

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ import ClinicalNotes from './Clinical-Notes';
 import Lab from './Lab';
 import MedList from './Med-List';
 import Phqo from './Phqo';
-import Reports from './Reports';
-import Radiology from './Radiology';
+import Reports from './Reports'
 
 
 
@@ -95,7 +94,7 @@ class App extends Component {
                     <div className="underline" />
                   </li>
                   <li className="nav-item">
-                    <NavLink className="nav-link" to="/Radiology">Radiology</NavLink>
+                    <NavLink className="nav-link" to="/radiology">Radiology</NavLink>
                     <div className="underline" />
                   </li>
                   <li className="nav-item">
@@ -237,7 +236,6 @@ class App extends Component {
                 <Route path="/med-list" component={MedList}/>
                 <Route path="/phqo" component={Phqo}/>
                 <Route path="/reports" component={Reports}/>
-                <Route path="/radiology" component={Radiology}/>
               </div>
             </HashRouter>
           </div>
